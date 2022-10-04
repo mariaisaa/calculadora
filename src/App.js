b90028c1b7a35539b8fc8d05d38755634843aad8
mariaisaa <mariacifuentescc@gmail.com>
2022-10-04 18:56:13 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Boton from './componentes/Boton'
 import Pantalla from './componentes/Pantalla'
 import BotonClear from './componentes/BotonClear';
 import { useState } from 'react';
+import { evaluate } from 'mathjs';
 
 
 function App() {
@@ -12,6 +13,12 @@ function App() {
 
     const agregarInput = val => {
       setInput (input + val);
+    };
+
+    const calcularResultado = () => {
+      if (input)
+      setInput ( evaluate(input));
+      return 'Por favor ingrese los valores para realizar los cálculos'
     };
 
 
@@ -46,7 +53,7 @@ function App() {
         <Boton manejarClic= {agregarInput}> * </Boton>
       </div>
       <div className= 'fila'> 
-        <Boton manejarClic= {agregarInput}> = </Boton>
+        <Boton manejarClic= {calcularResultado}> = </Boton>
         <Boton manejarClic= {agregarInput}> 0 </Boton>
         <Boton manejarClic= {agregarInput}> . </Boton>
         <Boton manejarClic= {agregarInput}> / </Boton>
